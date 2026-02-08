a0abada60a7f594907f87c48f9c20f48235ed5b6
Stas <122303615+Stasss1985@users.noreply.github.com>
2026-02-08 11:32:13 +0300

    add autorization and registration

---
+++ playwright_registration.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    # Открываем браузер Chromium (не в headless режиме, чтобы видеть действия)
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # Переходим на страницу регистрации
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    # Находим поле "Email" и заполняем его
+    email_fild = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_fild.fill("user.name@gmail.com")
+
+    # Находим поле "username" и заполняем его
+    username_fild = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_fild.fill("username")
+
+    # Находим поле "Password" и заполняем его
+    password_fill = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_fill.fill("password")
+
+    # Находим кнопку "Registration" и кликаем на нее
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # После перехода ищем заголовок "Dashboard" и сравниваем текст заголовка
+    dashboard_title_name = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title_name).to_be_visible()
+    expect(dashboard_title_name).to_have_text('Dashboard')
+
+    page.wait_for_timeout(3000)
