9afa5620fa2318adcd97f8d6aeb8e4975df18909
Stas <122303615+Stasss1985@users.noreply.github.com>
2026-08-14 10:03:14 +0300

    Added run autotests on CI

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -7,13 +7,14 @@ def create_allure_environment_file():
     # Создаем список из элементов в формате {key}={value}
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
     properties = '\n'.join(items)
+    # Здесь мы формируем список из ключей и значений наших настроек.
+    # Метод settings.model_dump() вернет нам настройки в формате словаря, что позволит удобно
+    # итерироваться по нему и создавать строку для environment.properties
 
     my_platform = f'os_info={platform.system()}, {platform.release()}'
 
     my_system = f'python_version={sys.version}'
-    # Здесь мы формируем список из ключей и значений наших настроек.
-    # Метод settings.model_dump() вернет нам настройки в формате словаря, что позволит удобно
-    # итерироваться по нему и создавать строку для environment.properties
+
     # Собираем все элементы в единую строку с переносами
     all_environment = properties + '\n' + my_platform + '\n' + my_system
 
